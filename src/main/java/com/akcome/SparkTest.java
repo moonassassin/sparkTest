ad8377a930eaf9d36a326b8e113d3a98e52e15be
shejiyuan <shejiyuan@akcome.com>
2018-05-18 10:31:27 +0800

    init

---
+++ src/main/java/com/akcome/SparkTest.java
@@ -0,0 +1,57 @@
+package com.akcome;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.spark.SparkContext;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.SparkSession;
+
+import scala.Tuple2;
+
+public class SparkTest {
+
+	private static final Pattern SPACE = Pattern.compile(" ");
+
+	public static void main(String[] args) {
+
+		SparkSession spark = SparkSession.builder().appName("JavaWordCount").getOrCreate();
+		SparkContext sparkContext = spark.sparkContext();
+
+		JavaRDD<String> lines = sparkContext.textFile("hdfs://master1:9000/tmp/1.text", 3).toJavaRDD();
+
+		JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+			@Override
+			public Iterator<String> call(String s) {
+				return Arrays.asList(SPACE.split(s)).iterator();
+			}
+		});
+
+		JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
+			@Override
+			public Tuple2<String, Integer> call(String s) {
+				return new Tuple2<>(s, 1);
+			}
+		});
+
+		JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+			@Override
+			public Integer call(Integer i1, Integer i2) {
+				return i1 + i2;
+			}
+		});
+
+		List<Tuple2<String, Integer>> output = counts.collect();
+		for (Tuple2<?, ?> tuple : output) {
+			System.out.println(tuple._1() + ": " + tuple._2());
+			System.out.println(args);
+		}
+		spark.stop();
+	}
+}
